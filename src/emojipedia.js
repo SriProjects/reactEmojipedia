8f15c62b61f0502fd36c62340d2d8029dc89f149
Sridevi R V <bunobrains@gmail.com>
2020-07-02 22:10:09 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "😍",
+    name: "Smiling Face With Heart-eyes Emoji",
+    meaning: "A symbol for when someone loves the idea of something."
+  },
+  {
+    id: 4,
+    emoji: "💘",
+    name: "Heart With Arrow Emoji",
+    meaning: "Cupid you have fallen in love."
+  },
+  {
+    id: 5,
+    emoji: "😂",
+    name: "Face With Tears Of Joy Emoji",
+    meaning: "Crying cause what you said is hilarious."
+  },
+  {
+    id: 6,
+    emoji: "😊",
+    name: "Smiling Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and a broad, closed smile turning up to rosy cheeks. Often expresses genuine happiness and warm, positive feelings."
+  }
+];
+
+export default emojipedia;
