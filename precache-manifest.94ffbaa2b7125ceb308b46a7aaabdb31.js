2e19195f6ed40696dc831caa52fd9ad8f958838f
Sridevi R V <bunobrains@gmail.com>
2020-07-03 19:28:33 +0530

    Updates

---
+++ precache-manifest.94ffbaa2b7125ceb308b46a7aaabdb31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745b42470b67912ce65f2ac28812a55b",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "ecb3ac9c5b65dffb42a7",
+    "url": "/Emojipedia/static/js/2.e9b4a3dd.chunk.js"
+  },
+  {
+    "revision": "12c37ff0909565b53cfc",
+    "url": "/Emojipedia/static/js/main.6b00d094.chunk.js"
+  },
+  {
+    "revision": "c0b58880861f3662b7a4",
+    "url": "/Emojipedia/static/js/runtime-main.ea709130.js"
+  }
+]);
