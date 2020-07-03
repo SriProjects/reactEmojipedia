4350cf302d864a796f64f391cf3e7961fe3f980f
Sridevi R V <bunobrains@gmail.com>
2020-07-03 20:50:05 +0530

    Updates

---
+++ precache-manifest.eb4c513d4d17823e02aa1a0dbae9e2e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ed5117ef15ed21a59056f2f172072c",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "ecb3ac9c5b65dffb42a7",
+    "url": "/Emojipedia/static/js/2.e9b4a3dd.chunk.js"
+  },
+  {
+    "revision": "12c37ff0909565b53cfc",
+    "url": "/Emojipedia/static/js/main.6b00d094.chunk.js"
+  },
+  {
+    "revision": "c0b58880861f3662b7a4",
+    "url": "/Emojipedia/static/js/runtime-main.ea709130.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa59bede052b890d7369020a03fbec13.js"
+  "/Emojipedia/precache-manifest.eb4c513d4d17823e02aa1a0dbae9e2e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Emojipedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
